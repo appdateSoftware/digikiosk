398e0d9b67510f12c863359f9f1e65d7aff70152
appdateSoftware <appdate.gr@gmail.com>
2024-03-24 15:32:24 +0200

    OK

--- src/screens/home.js
+++ src/screens/home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import {
   StyleSheet,
   View,
-  FlatList,
+  ScrollView,
   ActivityIndicator,
   BackHandler,
   Alert,
@@ -13,28 +13,49 @@ import Text from '../components/Text';
 import NumericKeyboard from '../components/NumericKeyboard';
 
 import Card from '../components/buttons/Card';
+import TouchableItem from "../components/TouchableItem";
 import {getStatusBarHeight} from 'react-native-status-bar-height';
 import {shadowDefault} from '../utils/shadow';
 import axios from 'axios';
 import Colors from "../theme/colors";
+import {useRealm, useQuery } from '@realm/react';
 
 import { inject, observer } from "mobx-react";
 
 import useTranslate from '../hooks/useTranslate';
 
+const colorsArray = [{
+  "id" : "blue",
+  "value" : Colors.primaryColor
+}, {
+  "id" : "turquize",
+  "value" : Colors.accentColor
+}, {
+  "id" : "red",
+  "value" : Colors.tertiaryColor
+}, {
+  "id" : "light yellow",
+  "value" : Colors.overlayColor
+}, {
+  "id" : "orange",
+  "value" : Colors.selection
+}, {
+  "id" : "black",
+  "value" : Colors.black
+}, {
+  "id" : "dark blue",
+  "value" : Colors.primaryColorDark
+}]
+
 const HomeScreen = ({navigation, route, feathersStore}) => { 
+
+  const realm = useRealm();
+  const realm_sections = useQuery('Section');
 
   let common = useTranslate(feathersStore.language);
 
-  const [deliveries, setDeliveries] = useState([]);
-  const [countStatus, setCountStatus] = useState({
-    pending: 0,
-    delivered: 0,
-  });
   const [loading, setLoading] = useState(true);
-  const [status, setStatus] = useState('Pending'); 
-
- 
+  const [section, setSection] = useState(1); 
 
   useEffect( () => { //Check for updates  
     checkForUpdates();
@@ -73,9 +94,7 @@ const HomeScreen = ({navigation, route, feathersStore}) => {
 
  const goToPlayStore = () => {
     Linking.openURL("https://play.google.com/store/apps/details?id=com.bringfood_db_android")
-  }
-  
-  
+  }  
 
   const renderFooter = () => {
     if (!loading) {
@@ -84,43 +103,66 @@ const HomeScreen = ({navigation, route, feathersStore}) => {
     return <ActivityIndicator animating size="small" />;
   };
 
- 
+  const sectionBtnPressed = (item) => {
+    setSection(item);
+  }
 
-    
+  const renderSectionItem = (item, index) => (
+    <View
+      key={index}
+      style={[styles.card, {backgroundColor: findColor(item.color)}]}
+    >
+      <TouchableItem
+        onPress={sectionBtnPressed(item)}
+        style={styles.cardContainer}
+        // borderless
+      >
+        <Text style={styles.cardTitle}>{item.name}</Text>
+        
+      </TouchableItem>        
+    </View>
+  );
+
+  const findColor = (id) => {
+    return colorsArray.find(color => color.id === id)?.value || ""
+  }
    
-     const headerComponent = () => (
-      <View style={styles.header}>
-        {feathersStore?.newVersion &&  
-          <Text onPress={goToPlayStore} style={styles.newVersion}>
-            {common?.newVersion}
+  const headerComponent = () => (
+    <View style={styles.header}>
+      {feathersStore?.newVersion &&  
+        <Text onPress={goToPlayStore} style={styles.newVersion}>
+          {common?.newVersion}
+        </Text>
+      }   
+
+    </View>
+  );
+
+  return ( 
+    <> 
+
+      <View style={styles.viewEmpty}>
+        {headerComponent()}
+
+        <Card style={styles.cardEmpty}>
+          {require('../assets/img/empty.png') && <Image source={require('../assets/img/empty.png')} />}
+          <Text third medium h3 h3Style={styles.textEmpty}>
+            {common.textEmpty}
           </Text>
-        } 
-       
-              
-
+        </Card>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          alwaysBounceHorizontal={false}
+          contentContainerStyle={styles.sectionsList}
+        >
+          {realm_sections?.map(( item, index ) => renderSectionItem(item, index))}
+        </ScrollView>  
+        <NumericKeyboard/>
       </View>
-    );
-
-   
-
-    return ( 
-      <>
-    
-     
-        <View style={styles.viewEmpty}>
-          {headerComponent()}
-
-          <Card style={styles.cardEmpty}>
-            {require('../assets/img/empty.png') && <Image source={require('../assets/img/empty.png')} />}
-            <Text third medium h3 h3Style={styles.textEmpty}>
-              {common.textEmpty}
-            </Text>
-          </Card>
-          <NumericKeyboard/>
-        </View>
-    
+  
     </>
-    );
+  );
           
 }
 
@@ -174,6 +216,32 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     paddingVertical: 2, 
     marginBottom: 8,   
+  },
+  sectionsList: {
+    paddingTop: 4,
+    paddingRight: 16,
+    paddingLeft: 8
+  },
+  cardImg: { borderRadius: 4 },
+  card: {
+    marginLeft: 8,
+    width: 104,
+    height: 72,
+    borderRadius: 4
+  },
+  cardContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  cardTitle: {
+    padding: 12,
+    fontWeight: "500",
+    fontSize: 16,
+    color: Colors.white,
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10
   },
 });
 
